6f45114ca7e81a3bd0851772f05ff0b9d5f0d467
Pogijames <143373570+Pogijames@users.noreply.github.com>
2026-03-01 19:24:07 +0800

    Add files via upload
    dsadasdasdasdasdas

---
+++ python/ermac.py
@@ -0,0 +1,7 @@
+# Hello World Program
+print("Hello, World!")
+
+# Personal Introduction
+print("My name is James Nicolai C. Ermac.")
+print("I am learning Python programming.")
+print("Nice to meet you!")
